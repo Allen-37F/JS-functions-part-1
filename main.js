2bb58c72a5b46516a44fa606a81cc8749cfba0d5
Allen Fordham <allen.s.fordham@gmail.com>
2016-07-13 13:17:13 -0600

    one through six completed

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ function isEqual(a, b) {
 console.log(isEqual(4, 5))
 
 //exercise 3
-// Define a function called `discountPercentage` that returns the total discount ($), given the original amount and discount percentage (as arguments). Return a warning if the discount amount is greater than 100 or less that 0 percent.
 function discountPercentage(amt, discount) {
   if (discount>100 || discount<0) {
     console.log("Invalid discount, brah")
@@ -26,7 +25,6 @@ function discountPercentage(amt, discount) {
 discountPercentage(50, 20)
 
 //exercise 4
-// Define a function called `stringCapitalize` that takes a string as an argument and returns the same string with the first letter of each word capitalized. Assume that each individual word is separated by only one space.
 function stringCapitalize(str) {
 
     console.log(str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}));
@@ -34,11 +32,28 @@ function stringCapitalize(str) {
 stringCapitalize("i have a cat")
 
 //exercise 5
-// Define a function called `evenNumbers` that takes an arbitrary integer as an argument, from 0 to 100, and outputs all the even integers from 0 to that arbitrary number.
 function evenIntegers(num) {
   for (i = 0; i <= num; i+=2) {
     if (i % 2 === 0) {console.log(i);
     }
-} 
+}
 }
 console.log(evenIntegers(34))
+
+//exercise 6
+function isDivisible(var1, var2) {
+  if (var1 % var2 === 0) {
+    return(true)
+  }
+  else {
+    return(false)
+  }
+}
+console.log(isDivisible(9, 3))
+console.log(isDivisible(9, 2))
+
+//exercise 7
+// Write a function called `oddNumbers` that takes an arbitrary integer as an argument, from 0 to 100. If the number is greater than 40 then output all the odd integers from 40 to the arbitrary number. If the number is less than 40, output all the odd integers from 0 to 40.
+function oddNumbers (int) {
+  
+}
